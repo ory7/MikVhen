0888c968cf1a1e96cc034ab4d907e7bc0745f035
josh <spoerri@gmail.com>
2024-03-29 10:36:28 -0400

    csv export support

--- admin.py
+++ admin.py
@@ -8,11 +8,31 @@ from . import views
 
 from django.http import HttpResponse
 import logging
+import csv
 
 logger = logging.getLogger(__name__)
 
-class AppointmentAdmin(admin.ModelAdmin):
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+class AppointmentAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ("Datetime", "Created_at", "private_contact")
+    actions = ["export_as_csv"]
 
 class MikvahAdmin(admin.AdminSite):
     site_title = "Mikvah Admin"
